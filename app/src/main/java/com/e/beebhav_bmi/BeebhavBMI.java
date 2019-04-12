c6c9d6ef50a19bbfb468f5cfe0bdd83e09e4c84e
Softwarica College Lab <softwarica@softwarica.com>
2019-04-12 13:41:07 +0545

    This is first commit

---
+++ app/src/main/java/com/e/beebhav_bmi/BeebhavBMI.java
@@ -0,0 +1,34 @@
+package com.e.beebhav_bmi;
+
+public class BeebhavBMI{
+    private double height, weight, bmi;
+
+    public BeebhavBMI(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double Bmi(){
+        bmi = weight/(height/100 * height/100);
+        bmi = Math.round(bmi*100)/100.0D;
+        return bmi;
+
+    }
+    public String Category(){
+        if (bmi<18.5){
+            return "Underweight";
+        }
+        else if(bmi>18.5 && bmi<24.9){
+            return "Normal Weight";
+
+        }
+        else if(bmi>25 && bmi<29.9){
+            return "Overweight";
+
+        }
+        else{
+            return "Obesity";
+
+        }
+    }
+}
---
+++ app/src/main/java/com/e/beebhav_bmi/MainActivity.java
@@ -0,0 +1,51 @@
+package com.e.beebhav_bmi;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etHeight, etWeight;
+    TextView tvBmis;
+    Button btnCompute;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etHeight = findViewById(R.id.etHeight);
+        etWeight = findViewById(R.id.etWeight);
+        tvBmis = findViewById(R.id.tvBmis);
+        btnCompute = findViewById(R.id.btnCompute);
+
+        btnCompute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double height, weight, bmi;
+                height= Double.parseDouble(etHeight.getText().toString());
+
+                weight = Double.parseDouble(etWeight.getText().toString());
+
+                BeebhavBMI bmiCalculator= new BeebhavBMI(height, weight);
+                bmi= bmiCalculator.Bmi();
+                tvBmis.setText(Double.toString(bmi));
+
+                Toast.makeText(MainActivity.this, bmiCalculator.Category(), Toast.LENGTH_LONG).show();
+
+
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(btnCompute.getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
+
+
+            }
+        });
+    }
+}
